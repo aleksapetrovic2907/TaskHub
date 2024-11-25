6fe417258d1cb868d8b8acb0d411afdaf021d6ed
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-11-26 00:02:03 +0100

    Implement `ApplicationUser` class

---
+++ TaskHub/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskHub.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(255, ErrorMessage = "The first name must contain a maximum of 255 characters.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(255, ErrorMessage = "The last name must contain a maximum of 255 characters.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string FullName => $"{LastName}, {FirstName}";
+    }
+}
