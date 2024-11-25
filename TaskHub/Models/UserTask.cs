af75f6214263cfc3a3615bd434ccb9e8c15ac118
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-11-26 00:07:50 +0100

    Implement `UserTask` class

---
+++ TaskHub/Models/UserTask.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskHub.Models
+{
+    public class UserTask
+    {
+        [Key]
+        public Guid TaskId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+        [ValidateNever]
+        public string Status { get; set; }
+
+        public string? AssignedToId { get; set; }
+        [ValidateNever]
+        public ApplicationUser? AssignedTo { get; set; }
+    }
+}
