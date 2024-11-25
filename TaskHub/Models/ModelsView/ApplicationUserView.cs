c7175a26b5e9ed0f2a3a60bf6df2dda6b43041db
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-11-26 00:15:36 +0100

    Implement MV `ApplicationUserView`

---
+++ TaskHub/Models/ModelsView/ApplicationUserView.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskHub.Models.ModelsView
+{
+    public class ApplicationUserView
+    {
+        [ValidateNever]
+        public string Id { get; set; }
+        [Required]
+        [EmailAddress]
+        [Remote(action: "IsEmailInUse", "ApplicationUsers")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Password and confirmation do not match.")]
+        public string ConfirmPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string Role { get; set; }
+    }
+}
