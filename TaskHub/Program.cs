42a9fe756f53eb6ecf98ce31dab7a75b940601d9
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-11-26 00:33:27 +0100

    Add `DbContext` to `Program`

--- TaskHub/Program.cs
+++ TaskHub/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TaskHub.Data;
+
 namespace TaskHub
 {
     public class Program
@@ -8,6 +11,8 @@ namespace TaskHub
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
