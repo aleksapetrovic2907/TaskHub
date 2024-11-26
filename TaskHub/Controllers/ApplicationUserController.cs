e072f0c828aababca4c00cd2ae932126a56fc680
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-11-26 15:30:52 +0100

    Implement POST method `Login()`

--- TaskHub/Controllers/ApplicationUserController.cs
+++ TaskHub/Controllers/ApplicationUserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using TaskHub.Models;
+using TaskHub.Models.ModelsView;
 
 namespace TaskHub.Controllers
 {
@@ -18,6 +19,26 @@ namespace TaskHub.Controllers
         [HttpGet]
         public async Task<IActionResult> Login()
         {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(LogInView model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Uses the default SignInManager to sign in if the user passes the authentication.
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError("", "Invalid Login attempt");
+            }
+
             return View();
         }
     }
