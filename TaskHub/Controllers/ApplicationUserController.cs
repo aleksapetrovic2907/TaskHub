9aaf81502bb0d168afeef34c159219c43abba02d
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-11-26 01:07:09 +0100

    Implement controller `ApplicationUserController`

---
+++ TaskHub/Controllers/ApplicationUserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskHub.Controllers
+{
+    public class ApplicationUserController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+    }
+}
