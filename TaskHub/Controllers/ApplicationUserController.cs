af1f3c2c1f399c73afa578d8ef07723d35845393
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-11-26 15:25:05 +0100

    Generate constructor for `ApplicationUserController`

--- TaskHub/Controllers/ApplicationUserController.cs
+++ TaskHub/Controllers/ApplicationUserController.cs
@@ -8,6 +8,12 @@ namespace TaskHub.Controllers
     public class ApplicationUserController : Controller
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public ApplicationUserController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> Login()
