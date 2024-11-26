13d4516acb0c8156e6d469952c0caf0051495932
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-11-26 15:24:02 +0100

    Implement reference to `SignInManager<>` in `ApplicationUserController`

--- TaskHub/Controllers/ApplicationUserController.cs
+++ TaskHub/Controllers/ApplicationUserController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using TaskHub.Models;
 
 namespace TaskHub.Controllers
 {
     public class ApplicationUserController : Controller
     {
+        private readonly SignInManager<ApplicationUser> _signInManager;
         [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> Login()
