63214bbfeb189999d90b0c66f100df028b575cdb
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-12-24 00:03:13 +0100

    Add in Domain layer: `User` entity

---
+++ TaskHub.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskHub.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+        public ICollection<Task> Tasks { get; set; }
+    }
+}
