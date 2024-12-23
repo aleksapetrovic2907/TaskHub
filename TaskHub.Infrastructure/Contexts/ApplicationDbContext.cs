b16835936dcab3da89024a7e7da997da5d846f40
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-12-24 00:17:52 +0100

    Add in Infrastructure layer: `ApplicationDbContext` context

---
+++ TaskHub.Infrastructure/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskHub.Domain.Entities;
+using Task = TaskHub.Domain.Entities.Task;
+
+namespace TaskHub.Infrastructure.Contexts
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
