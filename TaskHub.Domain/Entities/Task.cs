a557460fdf2e7aa6d39c70bc08a66111c0804b01
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-12-23 23:58:32 +0100

    Add in Domain layer: `Task` entity

---
+++ TaskHub.Domain/Entities/Task.cs
@@ -0,0 +1,15 @@
+﻿using TaskStatus = TaskHub.Domain.Enums.TaskStatus;
+
+namespace TaskHub.Domain.Entities
+{
+    public class Task
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DueAt { get; set; }
+        public TaskStatus Status { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
