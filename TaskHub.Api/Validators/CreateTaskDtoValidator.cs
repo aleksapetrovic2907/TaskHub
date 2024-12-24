2806ba9c0e9571d7f4168ef27a83a4034462f689
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-12-25 00:08:36 +0100

    Add in Api layer: FluentValidation validators for `CreateTaskDto` and `UpdateTaskDto`

---
+++ TaskHub.Api/Validators/CreateTaskDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using TaskHub.Api.Models;
+
+namespace TaskHub.Api.Validators
+{
+    public class CreateTaskDtoValidator : AbstractValidator<CreateTaskDto>
+    {
+        public CreateTaskDtoValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is required");
+
+            RuleFor(task => task.DueAt)
+                .GreaterThan(DateTime.UtcNow).WithMessage("Due date must be in the future");
+        }
+    }
+}
---
+++ TaskHub.Api/Validators/UpdateTaskDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using TaskHub.Api.Models;
+
+namespace TaskHub.Api.Validators
+{
+    public class UpdateTaskDtoValidator : AbstractValidator<CreateTaskDto>
+    {
+        public UpdateTaskDtoValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is required");
+
+            RuleFor(task => task.DueAt)
+                .GreaterThan(DateTime.UtcNow).WithMessage("Due date must be in the future")
+                .When(task => task.DueAt.HasValue);
+        }
+    }
+}
