9828f81b24de6e38ac121929c2e59c26aa648bb6
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-12-24 01:48:54 +0100

    Update in Infrastructure layer: Database migration after configuring identity

--- TaskHub.Infrastructure/Migrations/20241224004021_ConfigureIdentityAndTaskRelationships.Designer.cs
+++ TaskHub.Infrastructure/Migrations/20241224004021_ConfigureIdentityAndTaskRelationships.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaskHub.Infrastructure.Contexts;
 
@@ -11,9 +12,11 @@ using TaskHub.Infrastructure.Contexts;
 namespace TaskHub.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241224004021_ConfigureIdentityAndTaskRelationships")]
+    partial class ConfigureIdentityAndTaskRelationships
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
