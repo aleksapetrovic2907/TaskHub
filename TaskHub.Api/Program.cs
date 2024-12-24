95b26b36c33f9ad2d5b3e3ad0d041ee0d1b31e36
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-12-24 02:06:38 +0100

    Configuration in Api layer: Configure Swagger

--- TaskHub.Api/Program.cs
+++ TaskHub.Api/Program.cs
@@ -37,12 +37,17 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
