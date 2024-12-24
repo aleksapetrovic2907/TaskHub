2e8d1a2d1a22d4e30f05d3bcc1f98e6a63e54ff8
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-12-24 15:56:42 +0100

    Add in Api layer: `TasksController` for managing user tasks

---
+++ TaskHub.Api/Controllers/TasksController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using TaskHub.Application.Services;
+using TaskEntity = TaskHub.Domain.Entities.Task;
+
+namespace TaskHub.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class TasksController : ControllerBase
+    {
+        private readonly ITaskService _taskService;
+
+        public TasksController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTasks()
+        {
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var tasks = await _taskService.GetAllTasksAsync(userId);
+            return Ok(tasks);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetTaskById(Guid id)
+        {
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var task = await _taskService.GetTaskByIdAsync(userId, id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTask([FromBody] TaskEntity task)
+        {
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var createdTask = await _taskService.CreateTaskAsync(userId, task);
+            return CreatedAtAction(nameof(GetTaskById), new { id = createdTask.Id, createdTask });
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> UpdateTask(Guid id, [FromBody] TaskEntity updatedTask)
+        {
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            try
+            {
+                var targetTask = await _taskService.UpdateTaskAsync(userId, id, updatedTask);
+                return Ok(targetTask);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteTask(Guid id)
+        {
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+
+            try
+            {
+                await _taskService.DeleteTaskAsync(userId, id);
+                return NoContent();
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
