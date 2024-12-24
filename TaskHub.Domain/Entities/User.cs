47936fa4fc22489ff66f74ceaeca89cc8a7ee9d6
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-12-24 01:33:16 +0100

    Update in Domain layer: `User` inherits from `IdentityUser<Guid>`

--- TaskHub.Domain/Entities/User.cs
+++ TaskHub.Domain/Entities/User.cs
@@ -1,12 +1,10 @@
-﻿namespace TaskHub.Domain.Entities
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TaskHub.Domain.Entities
 {
-    public class User
+    public class User : IdentityUser<Guid>
     {
-        public Guid Id { get; set; }
         public string Name { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public string PasswordSalt { get; set; }
         public ICollection<Task> Tasks { get; set; }
     }
 }
