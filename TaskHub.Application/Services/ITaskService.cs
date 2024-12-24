8945ba0cf784117e4a11332ae84505af7ed65c42
aleksapetrovic2907 <66926460+aleksapetrovic2907@users.noreply.github.com>
2024-12-24 15:05:15 +0100

    Add in Application layer: `ITaskService` service

---
+++ TaskHub.Application/Services/ITaskService.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskHub.Application.Services
+{
+    public interface ITaskService
+    {
+        Task<IEnumerable<Domain.Entities.Task>> GetAllTasksAsync();
+        Task<Domain.Entities.Task> GetTaskByIdAsync(Guid userId, Guid taskId);
+        Task<Domain.Entities.Task> CreateTaskAsync(Guid userId, Domain.Entities.Task task);
+        Task<Domain.Entities.Task> UpdateTaskAsync(Guid userId, Guid taskId, Domain.Entities.Task task);
+        Task DeleteTaskAsync(Guid userId, Guid taskId);
+    }
+}
